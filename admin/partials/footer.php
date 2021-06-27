720b142d8cc8b5593f049b3b71c535bdbdcbb10e
SRIVARSHINI37 <69602535+SRIVARSHINI37@users.noreply.github.com>
2021-06-27 19:10:25 +0530

    Delete admin directory

--- admin/partials/footer.php
+++
@@ -1,9 +0,0 @@
-<!--Footer section starts --->
-<div class="footer">
-        <div class="wrapper">
-           <p class="text-center">2020 All rights reserved . Developed by-Sri Varshini</p>
-        </div>
-        </div>
-        <!--footer section starts --->
-    </body>
-</html>
--- admin/partials/login-check.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    //check whether user is logged in or not
-    //authorization or access control
-    if(!isset($_SESSION['user'])) // if user is not set
-    {
-        $_SESSION['no-login-message'] = "<div class='error text-center'>Please Login to Acces Admin Panel</div>";
-        header('location:'.SITEURL.'admin/login.php');
-    }
-?>
